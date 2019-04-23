b70eac17a2aafa968ad6261999baab172c030d6b
Master <butmalai.dan@gmail.com>
2019-04-23 23:56:16 +0300

    tema 2 pc makefile + libs
    tema 2 pc makefile + libs

---
+++ TemaPC2/server.cpp
@@ -0,0 +1,20 @@
+extern "C" {
+        #include <stdio.h>
+        #include <stdlib.h>
+        #include <unistd.h>
+        #include <string.h>
+        #include <sys/types.h>
+        #include <sys/socket.h>
+        #include <netinet/in.h>
+        #include <arpa/inet.h>
+        #include <netdb.h>
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        
+        return 0;
+}
