1fa1f2a82fe2d58c11f452b60113a9b127a05ec2
Dan Butmalai <dan.butmalai@xperi.com>
2019-11-18 16:40:42 +0200

    EGC Project implemented two modes

--- EGCProject/js/app.js
+++ EGCProject/js/app.js
@@ -22,11 +22,18 @@ var uniforms;
 var audio, analyser, mediaElement;
 
 //colors setup
-var floorColor = 0xBBDEF0, skyColor = 0xBBDEF0, cubesColor = 0xF08700;
+var skyColor = 0x808080, cubesColor = 0x00000;
 
-document.getElementById("menu").style.backgroundColor = "#BBDEF0";
+document.getElementById("menu").style.backgroundColor = "#808080";
 document.getElementById( 'play' ).addEventListener( 'click', clickedPlay);
 document.getElementById( 'pause' ).addEventListener( 'click', clickedPause);
+
+document.getElementById("mode").onchange = function(){
+    updateByMode();
+}
+document.getElementById("selector").onchange = function(){
+    clickedPlay();
+}
 
 initWorld();
 updateByMode();
@@ -86,16 +93,15 @@ function initWorld(){
     renderer.setClearColor( skyColor, 1 );
     document.body.appendChild( renderer.domElement );
 
-    //positionate camera
+    // controls
     controls = new THREE.OrbitControls( camera, renderer.domElement );
-    camera.position.set( -133.7817033929154, 95.38754075877799, 154.8839474041323);
-    camera.rotation.set(-0.5520030533709762, -0.6341343221091368, -0.34987573749653283);
 
-    //controls.update() must be called after any manual changes to the camera's transform
-    controls.update();
 }
 
 function initPoints(){
+    
+    clearPoints();
+
     var numParticles = AMOUNTX * AMOUNTY;
 
     var positions = new Float32Array( numParticles * 3 );
@@ -127,7 +133,7 @@ function initPoints(){
     var material = new THREE.ShaderMaterial( {
         
         uniforms: {
-            color: { value: new THREE.Color( 0x000000 ) },
+            color: { value: new THREE.Color(cubesColor) },
         },
         vertexShader: document.getElementById( 'vertexshader' ).textContent,
         fragmentShader: document.getElementById( 'fragmentshader' ).textContent
@@ -146,7 +152,8 @@ function initCubes(){
 
     //clear peviously existing cubes
     clearCubes();
-    
+    clearPoints();
+
     padding = 3;
     cubeNum = 27, cubeSide = 5;
     cubes = Array(cubeNum).fill().map(()=>Array(cubeNum).fill());
@@ -155,7 +162,7 @@ function initCubes(){
 
     var geometry = new THREE.BoxGeometry( cubeSide, cubeSide, cubeSide );
     var material = new THREE.MeshBasicMaterial( { color: cubesColor } );
-    var lineMaterial = new THREE.LineBasicMaterial({ color: 0xBBDEF0});
+    var lineMaterial = new THREE.LineBasicMaterial({ color: skyColor });
             
     for(var i = 0 ; i < cubeNum; i++){
         for(var j = 0 ; j < cubeNum; j++){            
@@ -176,26 +183,40 @@ function initCubes(){
 
 }
 
-document.getElementById("mode").onchange = function(){
-    updateByMode();
-}
-
 function updateByMode(){
 
     clearCubes();
     clearPoints();
     var mode = document.getElementById("mode").value;
-    switch (mode){
-        case "1":
+    console.log("mode = ");
+    console.log(mode);
+    if (mode == "cubes"){
+            console.log("1");
+            console.log(mode);
+            console.log(camera.position);
             initCubes();
-        case "2":
-            initPoints();   
-            break;
-        case "3":
-            break;
-        case "4":
-            break;    
+            //{x: -102.9448543720884, y: 119.76030964078583, z: 163.86472355814035}
+            //{_x: -0.638327321350248, _y: -0.521665272555228, _z: -0.35414285996149464, 
+            camera.position.set( -133.7817033929154, 95.38754075877799, 154.8839474041323);
+            camera.rotation.set(-0.5520030533709762, -0.6341343221091368, -0.34987573749653283);
+            //controls.update() must be called after any manual changes to the camera's transform
+            controls.update();
+            console.log(camera.position);
     }
+    if (mode == "points"){
+            console.log(camera.position);
+            console.log(mode);
+            console.log("2");
+            initPoints();   
+            // /camera = new THREE.PerspectiveCamera( 90, window.innerWidth/window.innerHeight, 0.1, 10000 );
+            camera.position.set(2016.155805055435, 1292.664447920684, 43.006988236302526);
+            camera.rotation.set(-1.5375385615942183, 1.0004142037624637, 1.531289459027026);
+            camera.aspect = 2.0869565217391304
+            //controls.update() must be called after any manual changes to the camera's transform
+            controls.update();
+            console.log(camera.position);
+    }
+    console.log(camera.position);
 }
 
 
@@ -206,16 +227,15 @@ var animate = function () {
         analyser.getFrequencyData();
         var data = analyser.data;
         var mode = document.getElementById("mode").value;
-        switch (mode){
-            case "1":
-                for (var i = 0 ; i < cubes.length; i++){
-                    for (var j = 0 ; j < cubes.length; j++){   
-                        cubes[i][j].scale.set( 1, Math.max(0.001, data[i+j]/4), 1);
-                        wireframe[i][j].scale.set( 1, Math.max(0.001, data[i+j]/4 ), 1);
-                    }
-                }          
-                break;
-            case "2":
+        if("cubes" == mode){
+            for (var i = 0 ; i < cubes.length; i++){
+                for (var j = 0 ; j < cubes.length; j++){   
+                    cubes[i][j].scale.set( 1, Math.max(0.001, data[i+j]/4), 1);
+                    wireframe[i][j].scale.set( 1, Math.max(0.001, data[i+j]/4 ), 1);
+                }
+            }        
+        }
+        if("points" == mode){
                 var positions = particles.geometry.attributes.position.array;
 				var scales = particles.geometry.attributes.scale.array;
 
@@ -240,17 +260,10 @@ var animate = function () {
 
 				particles.geometry.attributes.position.needsUpdate = true;
 				particles.geometry.attributes.scale.needsUpdate = true;
-				count += 0.1;
-                break;
-            case "3":
-
-                break;
-            case "4":
-                break;    
+                count += 0.1;
         }
-        renderer.render( scene, camera );
     }
+    renderer.render( scene, camera );  
 };
 
 animate();
-
